19acb5c1dc5755ada325f94194726115081baebe
IshaanGuleria <156114883+IshaanGuleria@users.noreply.github.com>
2025-05-01 17:50:35 +0530

    Update proc_load_bronze.sql

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -14,111 +14,125 @@ Parameters:
 
 Usage Example:
     EXEC bronze.load_bronze;
-===============================================================================*/
-create or alter procedure bronze.load_bronze as
-begin 
+===============================================================================
+*/
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
 
-	begin try
-	BEGIN TRANSACTION;
+		PRINT '------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '------------------------------------------------';
 
-	IF OBJECT_ID('bronze.crm_cust_info') IS NOT NULL
-		Truncate table bronze.crm_cust_info;
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
-			PRINT '------------------------------------------------';
-			PRINT 'Loading CRM Tables crm_cust_info';
-			PRINT '------------------------------------------------';
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
 
-	bulk insert bronze.crm_cust_info from 
-	'C:\Users\ESHAAN\OneDrive\Desktop\project_1\sql_data_warehouse_project\datasets\source_crm\cust_info.csv'
-	with(
-	Firstrow = 2,
-	fieldterminator=',',
-	tablock)
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\sql\dwh_project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\sql\dwh_project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
-	IF OBJECT_ID('bronze.crm_prd_info') IS NOT NULL
-		Truncate table bronze.crm_prd_info;
-	
-			PRINT '------------------------------------------------';
-			PRINT 'Loading CRM Tables crm_prd_info';
-			PRINT '------------------------------------------------';
-
-	bulk insert bronze.crm_prd_info from 
-	'C:\Users\ESHAAN\OneDrive\Desktop\project_1\sql_data_warehouse_project\datasets\source_crm\prd_info.csv'
-	with(
-	Firstrow = 2,
-	fieldterminator=',',
-	tablock)
-	
-	IF OBJECT_ID('bronze.crm_sales_details') IS NOT NULL
-		Truncate table bronze.crm_sales_details;
-	
-		    PRINT '------------------------------------------------';
-			PRINT 'Loading CRM Tables crm_sales_details';
-			PRINT '------------------------------------------------';
-	
-	bulk insert bronze.crm_sales_details from 
-	'C:\Users\ESHAAN\OneDrive\Desktop\project_1\sql_data_warehouse_project\datasets\source_crm\sales_details.csv'
-	with(
-	Firstrow = 2,
-	fieldterminator=',',
-	tablock)
-	
-
-			PRINT '************************************************';
-			PRINT 'Loading erp Tables';
-			PRINT '************************************************';
-
-
-	IF OBJECT_ID('bronze.erp_cust_az12') IS NOT NULL
-		Truncate table bronze.erp_cust_az12;
-	
-			PRINT '------------------------------------------------';
-			PRINT 'Loading erp Tables erp_cust_az12 ';
-			PRINT '------------------------------------------------';	
+		PRINT '------------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '------------------------------------------------';
 		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\sql\dwh_project\datasets\source_erp\loc_a101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
-	bulk insert bronze.erp_cust_az12 from 
-	'C:\Users\ESHAAN\OneDrive\Desktop\project_1\sql_data_warehouse_project\datasets\source_erp\CUST_AZ12.csv'
-	with(
-	Firstrow = 2,
-	fieldterminator=',',
-	tablock)
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\sql\dwh_project\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
-	IF OBJECT_ID('bronze.erp_loc_a101') IS NOT NULL
-			Truncate table bronze.erp_loc_a101;
-			
-			PRINT '------------------------------------------------';
-			PRINT 'Loading erp Tables erp_loc_a101';
-			PRINT '------------------------------------------------';
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\sql\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
 
-	bulk insert bronze.erp_loc_a101 from 
-	'C:\Users\ESHAAN\OneDrive\Desktop\project_1\sql_data_warehouse_project\datasets\source_erp\LOC_A101.csv'
-	with(
-	Firstrow = 2,
-	fieldterminator=',',
-	tablock)
-	IF OBJECT_ID('bronze.erp_px_cat_g1v2') IS NOT NULL
-		Truncate table bronze.erp_px_cat_g1v2;
-			
-			PRINT '------------------------------------------------';
-			PRINT 'Loading erp Tables erp_px_cat_g1v2';
-			PRINT '------------------------------------------------';
-			
-	bulk insert bronze.erp_px_cat_g1v2 from 
-	'C:\Users\ESHAAN\OneDrive\Desktop\project_1\sql_data_warehouse_project\datasets\source_erp\PX_CAT_G1V2.csv'
-	with(
-	Firstrow = 2,
-	fieldterminator=',',
-	tablock)
-	COMMIT TRANSACTION;
-	print '========================'
-	print 'Data loaded successfully'
-	print '========================'
-
-	end try
-	begin catch
-	ROLLBACK TRANSACTION;
-	print 'Error Occured during loading the bronze layer'
-	end catch
-end
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT '=========================================='
+	END CATCH
+END
